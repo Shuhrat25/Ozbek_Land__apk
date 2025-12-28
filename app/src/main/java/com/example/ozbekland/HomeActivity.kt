e1e6ad5ad8c7343f2b9f7980b935394a270991be
Abdurahmonov Shuhrat <shabdurahmonov2004@gmail.com>
2025-12-28 17:08:33 +0500

    Shuhrat

--- app/src/main/java/com/example/ozbekland/HomeActivity.kt
+++ app/src/main/java/com/example/ozbekland/HomeActivity.kt
@@ -10,7 +10,6 @@ import com.example.ozbekland.ui.theme.OzbekLandTheme
 
 import androidx.compose.foundation.Image
 import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
